c4b7d1960fe584bc30365ecde5ad57347bb06275
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-07 22:00:10 +0200

    [UPD] Correzione commenti

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,9 +1,12 @@
 'use strict';
 
-// creazione variabile con prompt per inserimento della parola (.trim() per rimuovere gli spazi)
+// creazione variabile con prompt per inserimento della parola (.split(' ').join('') per rimuovere gli spazi)
 let input = prompt('inserisci la parola o la frase da verificare').toLocaleLowerCase().split(' ').join('');
 
-// creazione funzione per ribaltare una stringa
+// creazione funzione per ribaltare una stringa 
+// divindendola in un array momentaneo
+// ribaltando l'array momentaneo
+// ritrasformare l'array momentaneo in una stringa
 function operazioniRibaltamento(input) {
     return input.split('').reverse().join('');
 }
