44f8f33836c886d4c4c2c8b0a8cbbfe7255e9017
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-07 21:13:18 +0200

    [UPD] Richiesta inserimento dati e trasformazioni

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,2 +1,4 @@
 'use strict';
 
+// creazione variabile con prompt per inserimento della parola (.trim() per rimuovere gli spazi)
+let input = prompt('inserisci la parola o la frase da verificare').trim().toLowerCase();
