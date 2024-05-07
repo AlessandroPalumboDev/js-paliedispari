ad9853e64f4eb9f19db5f556de86e0b1fa0d12d1
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-07 21:16:19 +0200

    [UPD] definizione stringa e dichiarazione variabile con invocazione della funzione

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -2,3 +2,11 @@
 
 // creazione variabile con prompt per inserimento della parola (.trim() per rimuovere gli spazi)
 let input = prompt('inserisci la parola o la frase da verificare').trim().toLowerCase();
+
+// creazione funzione per ribaltare una stringa
+function operazioniRibaltamento(input) {
+    return input.split("").reverse().join("");
+}
+
+// dichiaro una variabile della funzione invocandola
+const ribaltato = operazioniRibaltamento(input);
